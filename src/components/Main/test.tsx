6564fb8d9557310435959b35b3991ad47ee9c5e0
natron <vitornovaes.santos@gmail.com>
2021-02-02 18:57:16 -0300

    build 1.0 boilerplate-next

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+//import 'jest-styled-components'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
+  })
+})
